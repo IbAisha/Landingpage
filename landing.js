6af6990581311207c403a105c09786d4c610ebc6
Aïsha <zawjatu.a@gmail.com>
2022-06-29 09:49:16 +0200

    commit

---
+++ landing.js
@@ -0,0 +1,26 @@
+const title = document.querySelector('h1');
+const txt = "search"
+
+
+
+function typewriter(word, index){
+
+    if(index < word.length) {
+
+        setTimeout(() => {
+
+            title.innerHTML += `<span>${word[index]}</span>`
+
+            typewriter(txt, index + 1)
+
+        }, 300);
+
+    }
+
+}
+
+setTimeout(() => {
+
+    typewriter(txt, 0)
+
+}, 500);
